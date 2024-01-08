5f2f369551212cbe69fc1ffce7d9b210f387bb9c
Mārtiņš Gunārs Volkovs <martinsvolkovs07@gmail.com>
2024-01-08 20:57:28 +0200

    Removal of unnecessary files, Creation of basic Controller and View

---
+++ CalculatorMVC/Controllers/CalculatorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorMVC.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return await Task.Run(() => View());
+        }
+    }
+}
